750bc2bae38f2a5a4c21cf3cbd357493b977045c
zjw <15768813324@163.com>
2020-08-12 08:19:22 +0800

    修改了socket

--- src/main/java/com/zjw/tradeplatformv1/controller/WebSocketController.java
+++ src/main/java/com/zjw/tradeplatformv1/controller/WebSocketController.java
@@ -21,25 +21,22 @@ public class WebSocketController {
     SimpMessagingTemplate template;
     /**
      * @Description:这个方法是接收客户端发送功公告的WebSocket请求，使用的是@MessageMapping
-     * @Author:hui.yunfei@qq.com
      * @Date: 2019/5/31
      */
     @MessageMapping("/transfer")//客户端访问服务端的时候config中配置的服务端接收前缀也要加上 例：/app/change-notice
     public void greeting(Message message){
         System.out.println("服务端接收到消息："+message.toString());
 
-        send(message.getHeaders().toString());
-
-        //我们使用这个方法进行消息的转发发送！
-        /*template.convertAndSend("/topic/notice", value);(可以使用定时器定时发送消息到客户端)
-                @Scheduled(fixedDelay = 1000L)
-                public void time() {
-                    template.convertAndSend("/system/time", new Date().toString());
-               }*/
+        send(message.getHeaders().toString(),message.getPayload().toString());
         return;
     }
 
-    public void send(String name){
-        template.convertAndSendToUser(name,"/topic/message","bangbang");
+    /**
+     * 转发给指定用户
+     * @param name
+     * @param context
+     */
+    public void send(String name,String context){
+        template.convertAndSendToUser(name,"/topic/message",context);
     }
 }
