87c479e653b62fa174c8df6d980e7f8e7ea330ad
zjw <15768813324@163.com>
2020-09-06 08:57:45 +0800

    系统化了各个模块

--- src/main/java/com/zjw/tradeplatformv1/Tradeplatformv1Application.java
+++ src/main/java/com/zjw/tradeplatformv1/Tradeplatformv1Application.java
@@ -2,17 +2,24 @@ package com.zjw.tradeplatformv1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
-public class Tradeplatformv1Application {
+public class Tradeplatformv1Application  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tradeplatformv1Application.class, args);
 	}
 
+	/*@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(Tradeplatformv1Application.class);
+	}*/
 
 }
--- src/main/java/com/zjw/tradeplatformv1/service/AliPayService.java
+++
@@ -1,7 +0,0 @@
-package com.zjw.tradeplatformv1.service;
-
-import com.alipay.api.AlipayApiException;
-
-public interface AliPayService {
-    String aliPay(Integer amount) throws AlipayApiException;
-}
